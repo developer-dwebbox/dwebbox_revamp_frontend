c502ef3885a89e8ee84c26ea84c3b7cc022bf7c8
syed-dwebbox <syed.maaz@dwebbox.com>
2024-03-06 12:30:21 +0530

    anim-marquee

--- src/routes/Ourteams/Ourteamimages.js
+++ src/routes/Ourteams/Ourteamimages.js
@@ -17,31 +17,31 @@ function Ourteamimages() {
               <div className='overlay-anim'>
               <div className="marquee-overlay">
   <ul className="marquee__contentoverlay scroll">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
   <ul className="marquee__contentoverlay scroll" aria-hidden="true">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
 </div>
 
 <div className="marquee-overlayTwo">
   <ul className="marquee__contentoverlayTwo scroll">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
   <ul className="marquee__contentoverlayTwo scroll" aria-hidden="true">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
 </div>
 
@@ -59,31 +59,31 @@ function Ourteamimages() {
               <div className='overlay-anim'>
               <div className="marquee-overlay">
   <ul className="marquee__contentoverlay scroll">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
   <ul className="marquee__contentoverlay scroll" aria-hidden="true">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
 </div>
 
 <div className="marquee-overlayTwo">
   <ul className="marquee__contentoverlayTwo scroll">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
   <ul className="marquee__contentoverlayTwo scroll" aria-hidden="true">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
 </div>
 
@@ -101,31 +101,31 @@ function Ourteamimages() {
               <div className='overlay-anim'>
               <div className="marquee-overlay">
   <ul className="marquee__contentoverlay scroll">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
   <ul className="marquee__contentoverlay scroll" aria-hidden="true">
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
-  <p>Co Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
+  <p>Co-Founder</p>
   </ul>
 </div>
 
 <div className="marquee-overlayTwo">
   <ul className="marquee__contentoverlayTwo scroll">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
   <ul className="marquee__contentoverlayTwo scroll" aria-hidden="true">
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
-  <p>Kushal Shah</p>
-  <p>Kushal shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
+  <p className='dwb-dev-name' >Kushal Shah</p>
   </ul>
 </div>
 
