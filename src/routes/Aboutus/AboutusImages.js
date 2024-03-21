3f0362a09059c12d3b1a3b5661eacdadbd98b9d7
syed-dwebbox <syed.maaz@dwebbox.com>
2024-03-21 10:49:22 +0530

    clientsimageupdate

--- src/routes/Aboutus/AboutusImages.js
+++ src/routes/Aboutus/AboutusImages.js
@@ -8,6 +8,15 @@ import Imagefive from '../../../src/Assets/Images/OurTeam/ImageFive.png'
 import Imagesix from '../../../src/Assets/Images/OurTeam/ImageSix.png'
 import Imageseven from '../../../src/Assets/Images/OurTeam/ImageSeven.png'
 import Imageeight from '../../../src/Assets/Images/OurTeam/ImageEight.png'
+import Imagenine from '../../../src/Assets/Images/OurTeam/ImageNine.png'
+import Imageten from '../../../src/Assets/Images/OurTeam/ImageTen.png'
+import Imageeleven from '../../../src/Assets/Images/OurTeam/ImageEleven.png'
+import Imagetwelve from '../../../src/Assets/Images/OurTeam/ImageTwelve.png'
+import Imagethirteen from '../../../src/Assets/Images/OurTeam/ImageThirteen.png'
+import Imagefourteen from '../../../src/Assets/Images/OurTeam/ImageFourteen.png'
+import Imagefifteen from '../../../src/Assets/Images/OurTeam/ImageFifteen.png'
+
+
 
 
 
@@ -17,16 +26,16 @@ function AboutusImages() {
    <div className="marqueeTeam">
   <ul className="marquee__contentTeam scrollTeam">
     <li>
-      <img src={Imageone} alt="" width="90%"  />
+      <img src={Imagenine} alt="" width="90%"  />
     </li>
     <li>
-      <img src={Imagetwo} alt="" width="90%"  />
+      <img src={Imageten} alt="" width="90%"  />
     </li>
     <li>
-   <img src={Imagethree} alt="" width="90%"  />
+   <img src={Imageeleven} alt="" width="90%"  />
     </li>
     <li>
-    <img src={Imagefour} alt="" width="90%"  />
+    <img src={Imagetwelve} alt="" width="90%"  />
     </li>
   </ul>
   <ul className="marquee__contentTeam scrollTeam">
@@ -64,13 +73,13 @@ function AboutusImages() {
   <ul className="marquee__contentTeamTwo scrollTeamtwo">
   
   <li>
-      <img src={Imagefive} alt="" width="90%"  />
+      <img src={Imagethirteen} alt="" width="90%"  />
     </li>
     <li>
-      <img src={Imagesix} alt="" width="90%"  />
+      <img src={Imagefourteen} alt="" width="90%"  />
     </li>
     <li>
-   <img src={Imageseven} alt="" width="90%"  />
+   <img src={Imagefifteen} alt="" width="90%"  />
     </li>
     <li>
     <img src={Imageeight} alt="" width="90%"  />
--- src/routes/HomePage/Ourclients/Ourclients.js
+++ src/routes/HomePage/Ourclients/Ourclients.js
@@ -30,25 +30,62 @@ import ClientImage22 from '../../../Assets/Images/ClientsImages/Group 18420.png'
 import ClientImage23 from '../../../Assets/Images/ClientsImages/Group 18421.png';
 import ClientImage24 from '../../../Assets/Images/ClientsImages/Group 18425.png';
 import ClientImage25 from '../../../Assets/Images/ClientsImages/Group 18426.png';
+import ClientImage26 from '../../../Assets/Images/ClientsImages/Group 18440.png';
+import ClientImage27 from '../../../Assets/Images/ClientsImages/Group 18441.png';
+import ClientImage28 from '../../../Assets/Images/ClientsImages/Group 18442.png';
+import ClientImage29 from '../../../Assets/Images/ClientsImages/Group 18443.png';
+import ClientImage30 from '../../../Assets/Images/ClientsImages/Group 18445.png';
+import ClientImage31 from '../../../Assets/Images/ClientsImages/Group 18465.png';
+import ClientImage32 from '../../../Assets/Images/ClientsImages/Group 18473.png';
+import ClientImage33 from '../../../Assets/Images/ClientsImages/lock and key.png';
+
+import ClientImage34 from '../../../Assets/Images/ClientsImages/Group 1272628233.png';
+import ClientImage35 from '../../../Assets/Images/ClientsImages/Group 1272628235.png';
+import ClientImage36 from '../../../Assets/Images/ClientsImages/Group 1272628239.png';
+import ClientImage37 from '../../../Assets/Images/ClientsImages/Group 1272628244.png';
+import ClientImage38 from '../../../Assets/Images/ClientsImages/Group 1272628252.png';
+import ClientImage39 from '../../../Assets/Images/ClientsImages/Group 1272628255.png';
+import ClientImage40 from '../../../Assets/Images/ClientsImages/Group 1272628258.png';
+import ClientImage41 from '../../../Assets/Images/ClientsImages/Group 1272628258.png';
+import ClientImage42 from '../../../Assets/Images/ClientsImages/Group 1272628259.png';
+import ClientImage43 from '../../../Assets/Images/ClientsImages/Group 1272628266.png';
+import ClientImage44 from '../../../Assets/Images/ClientsImages/Group 1272628267.png';
+import ClientImage45 from '../../../Assets/Images/ClientsImages/Group 1272628268.png';
+import ClientImage46 from '../../../Assets/Images/ClientsImages/Group 1272628270.png';
+import ClientImage47 from '../../../Assets/Images/ClientsImages/Group 1272628274.png';
+import ClientImage48 from '../../../Assets/Images/ClientsImages/Group 1272628276.png';
+import ClientImage49 from '../../../Assets/Images/ClientsImages/Group 1272628278.png';
+import ClientImage50 from '../../../Assets/Images/ClientsImages/Group 1272628280.png';
+
+
+
+
+
+
 
 
 
 function Ourclients() {
+  const [shuffledImages, setShuffledImages] = useState([]);
   // Define an array containing all client images
   const clientImages = [
     ClientImage1, ClientImage2, ClientImage3, ClientImage4, ClientImage5,
     ClientImage6, ClientImage7, ClientImage8, ClientImage9, ClientImage10,
     ClientImage11, ClientImage12, ClientImage13, ClientImage14, ClientImage15,
     ClientImage16, ClientImage17, ClientImage18, ClientImage19, ClientImage20,
-    ClientImage21,ClientImage22,ClientImage23,ClientImage24,ClientImage25
+    ClientImage21,ClientImage22,ClientImage23,ClientImage24,ClientImage25,
+    ClientImage26,ClientImage27,ClientImage28,ClientImage29,ClientImage30,
+    ClientImage31,ClientImage32,ClientImage33,ClientImage34,ClientImage35,
+    ClientImage36,ClientImage37,ClientImage38,ClientImage39,ClientImage40,
+    ClientImage41,ClientImage42,ClientImage43,ClientImage44,ClientImage45,
+    ClientImage46,ClientImage47,ClientImage48,ClientImage49,ClientImage50
   ];
 
-  const [shuffledImages, setShuffledImages] = useState([]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       // Shuffle the array of client images
-      const shuffled = clientImages.sort(() => Math.random() - 0.5).slice(0, 12);
+      const shuffled = clientImages.sort(() => Math.random() - 0.5).slice(0, 8);
       setShuffledImages(shuffled);
     }, 4000);
 
